2a325e88529556585c25effb7ffadb645d001634
JSShant <joshua.shanthikumar@outlook.com>
2022-08-24 12:20:46 +1000

    Create Components with external/inline styles/html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent], //Which component should you recognise in the index.html
---
+++ src/app/success/success.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-success',
+  template: `<p>Success</p>`,
+  styles: [
+    `
+      p {
+        color: lightgreen;
+      }
+    `,
+  ],
+})
+export class SuccessComponent {}
